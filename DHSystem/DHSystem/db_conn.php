f1bc14a6f49e0d88dfeac7bd2040639cd6f75bca
Wenie22 <60214598+Wenie22@users.noreply.github.com>
2023-04-01 09:52:01 +0000

    modified:   DHSystem/DHSystem/db_conn.php

--- DHSystem/DHSystem/db_conn.php
+++ DHSystem/DHSystem/db_conn.php
@@ -7,11 +7,13 @@
   $db_name = "railway";
   
   $conn = new mysqli($servername, $username, $password, $db_name);
-  
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
   else {
+    // set the connection timeout to 60 seconds
+    mysqli_options($conn, MYSQLI_OPT_CONNECT_TIMEOUT, 60);
+
     echo "success";
   }
 ?>
