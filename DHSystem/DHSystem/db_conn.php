105adfd3a968dd4dcd12c543f258f37762141541
Wenie22 <60214598+Wenie22@users.noreply.github.com>
2023-04-01 05:25:41 +0000

    modified:   DHSystem/DHSystem/db_conn.php

--- DHSystem/DHSystem/db_conn.php
+++ DHSystem/DHSystem/db_conn.php
@@ -1,12 +1,11 @@
 <?php
   $servername = "containers-us-west-176.railway.app";
-  $port = 7059;
   $username = "root";
   $password = "uF5UgfB1A5DbRb4LcZOA";
 
   $db_name = "railway";
   
-  $conn = new mysqli($servername, $port, $username, $password, $db_name);
+  $conn = new mysqli($servername, $username, $password, $db_name);
   
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
